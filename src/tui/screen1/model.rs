3530c7ca795b0b7ff206543858a28e298dd4f564
hardcoder <proteus.dial8891@gmail.com>
2025-10-21 03:40:40 +0530

    Added view implementation.

--- src/tui/screen1/model.rs
+++ src/tui/screen1/model.rs
@@ -42,6 +42,12 @@ pub struct ModelState {
 	pub tx : Sender<Option<()>>
 }
 
+async fn getNextTrack(h: polaris::polarisHandle, s: &HashSet<usize>) -> String {
+    let mut list_polaris = polaris::getIterator(h).await;
+    let index = s.iter().next().unwrap();
+    list_polaris.nth(*index).unwrap().1
+}
+
 impl<'c> Compute<'c> for Model {
     type State = State;
     type Output = Output;
@@ -75,14 +81,34 @@ impl<'c> Compute<'c> for Model {
 		    },
 
 			PlaybackFinished => {
-                eprintln!("<Model> : playback end detected.");
-				return Output::View(View { 
-				    data : self.data,
-				    cmd : PlayTrack(
-				        String::from(""),
-						state_data.list.clone(),
-						state_data.selection.clone(),
-                        state_data.playlist.clone()	) 
+				let curr_playlist = (*state_data.playlist).clone();
+				//eprintln!("<Model> : Curr Playlist len {}", curr_playlist.len());
+				let mut curr_iter = curr_playlist.iter();
+				//eprintln!("<Model> : Remaining iter length {}", curr_iter.len());
+				let index = curr_iter.next().unwrap();
+				//eprintln!("<Model> : playback end detected.");
+			    Rc::get_mut(&mut state_data.playlist).unwrap().remove(index);
+				
+				if state_data.toggle && !state_data.playlist.is_empty() {
+    				let next = getNextTrack(self.data.clone(), &state_data.playlist).await;
+					eprintln!("<Model> : Next track selected {}",next);
+    				return Output::View(View { 
+	    			    data : self.data,
+		    		    cmd : PlayTrack(
+			    	        next,
+				    		state_data.list.clone(),
+					    	state_data.selection.clone(),
+                            state_data.playlist.clone()	) 
+					    } 
+					);
+				} else 
+				if { state_data.toggle } {
+					state_data.toggle = false;
+				}
+
+				return Output::View(View {
+					data : self.data,
+					cmd : ViewNoop 
 					} 
 				);
 			},
@@ -148,10 +174,26 @@ impl<'c> Compute<'c> for Model {
             TogglePlay => {
                 state_data.toggle = !state_data.toggle;
 
-                eprintln!("<Model> : Play button toggled.");
-			    return Output::View(View {
-                    data : self.data,
-			        cmd : ViewNoop 
+				if state_data.toggle && !state_data.playlist.is_empty() {
+    				let next = getNextTrack(self.data.clone(), &state_data.playlist).await;
+					eprintln!("<Model> : Next track selected {}",next);
+    				return Output::View(View { 
+	    			    data : self.data,
+		    		    cmd : PlayTrack(
+			    	        next,
+				    		state_data.list.clone(),
+					    	state_data.selection.clone(),
+                            state_data.playlist.clone()	) 
+					    } 
+					);
+				} else 
+				if { state_data.toggle } {
+					state_data.toggle = false;
+				}
+
+				return Output::View(View {
+					data : self.data,
+					cmd : ViewNoop 
 					} 
 				);
 			},
--- src/tui/screen1/view.rs
+++ src/tui/screen1/view.rs
@@ -1,13 +1,21 @@
 use crate::tui;
 use crate::tui::screen1::{controller::Controller, model::Model,
-    ViewCommand::{self}};
+    ViewCommand::{self, Draw, PlayTrack}};
 use crate::tui::{Components, Compute, IntoComponent, IntoComp};
-use ratatui::DefaultTerminal;
 use rppal::gpio::{self, InputPin};
 use crate::tui::screen1::{State, Output};
 use crate::polaris::{self, polarisHandle};
 use std::rc::Rc;
 use std::sync::mpsc::{Sender, Receiver, channel};
+use std::collections::HashSet;
+
+use color_eyre::Result;
+use crossterm::event::{self, KeyCode};
+use ratatui::layout::{Constraint, Layout, Rect};
+use ratatui::style::{Color, Modifier, Style, Stylize};
+use ratatui::text::{Line, Span, Text};
+use ratatui::widgets::{List, ListDirection, ListItem, ListState, Paragraph};
+use ratatui::{DefaultTerminal, Frame};
 
 #[derive(Clone)]
 pub struct View {
@@ -22,6 +30,69 @@ pub struct ViewState {
 }
 
 
+/// Render the UI with various lists.
+fn render(
+    frame: &mut Frame,
+    list_state: &mut ListState,
+    list_model: &Vec<String>,
+    toggle_play: bool,
+    l_playlist: &HashSet<usize>,
+) {
+    use Constraint::{Fill, Length, Min};
+
+    let vertical = Layout::vertical([Length(8), Length(2)]);
+    let [top, bottom] = vertical.areas(frame.area());
+
+    //let title = Line::from_iter([
+    //    Span::from("List Widget").bold(),
+    //    Span::from(" (Press 'q' to quit and arrow keys to navigate)"),
+    //]);
+    //frame.render_widget(title.centered(), top);
+
+    render_list(frame, top, list_state, list_model, l_playlist);
+    render_bottom(frame, bottom, toggle_play);
+}
+
+const SELECTED_STYLE: Style = Style::new().add_modifier(Modifier::BOLD);
+
+/// Render a list.
+pub fn render_list(
+    frame: &mut Frame,
+    area: Rect,
+    list_state: &mut ListState,
+    list_model: &Vec<String>,
+    l_playlist: &HashSet<usize>,
+) {
+    let list =
+        List::new(list_model.into_iter().map(|x| x.as_str()).enumerate().map(
+            |(i, x)| {
+                if l_playlist.contains(&i) {
+                    ListItem::new(x).yellow()
+                } else {
+                    ListItem::new(x).white()
+                }
+            },
+        ))
+        //    .highlight_style(SELECTED_STYLE);
+        .highlight_style(Modifier::UNDERLINED);
+    frame.render_stateful_widget(list, area, list_state);
+}
+
+/// Render a bottom-to-top list.
+pub fn render_bottom(frame: &mut Frame, area: Rect, auto_play: bool) {
+    let final_text;
+    match auto_play {
+        false => final_text = String::from("\n             "),
+        true => {
+            let mut temp_text = String::from("\n             ");
+            temp_text.extend(["A"]);
+            final_text = temp_text;
+        }
+    }
+    let text = Paragraph::new(final_text);
+    frame.render_widget(text, area);
+}
+
 impl<'c> Compute<'c> for View {
     type State = State;
     type Output = Output;
@@ -32,6 +103,21 @@ impl<'c> Compute<'c> for View {
         terminal: &mut DefaultTerminal,
         _: [&'c InputPin; 6],
     ) -> Output {
-        Output::Controller(Controller { data : self.data })
+		
+		match self.cmd {
+		    PlayTrack(name, data, mut list_state, playlist) => {
+                terminal.draw(|frame| {
+				    render(frame, &mut list_state, &data, false, &playlist) } ).unwrap();
+            },
+			
+			Draw(data, mut list_state, playlist) => {
+                terminal.draw(|frame| {
+				    render(frame, &mut list_state, &data, false, &playlist) } ).unwrap();
+            },
+
+            _ => {}			
+		}
+		
+		Output::Controller(Controller { data : self.data })
     }
 }
