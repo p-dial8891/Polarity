e8ea497cc9224b50dca83f30c696f8c2c9e59397
Hardcoder <proteus.dial8891@gmail.com>
2026-03-29 21:57:28 +0100

    Reset input event for pgup and pgdown.

--- terminal/src/tui/home/foreground/controllers.rs
+++ terminal/src/tui/home/foreground/controllers.rs
@@ -224,12 +224,14 @@ impl Compute for Controller1 {
 			    cmd : ModelCommand::AddTrack	});
 		}
 		if let Some(Event::Key(e)) = input.ev {
+			input.ev = None;
 			if e == KeyCode::PageUp.into() && e.is_press() {
 				return Self::Output::Model(Model1 { data : self.data,
 			    	cmd : ModelCommand::PageUp	});
 			}
 		}
 		if let Some(Event::Key(e)) = input.ev {
+			input.ev = None;
 			if e == KeyCode::PageDown.into() && e.is_press() {
 				return Self::Output::Model(Model1 { data : self.data,
 			    	cmd : ModelCommand::PageDown	});
