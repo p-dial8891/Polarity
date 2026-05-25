ee5adc255307ec5bcc81fbc608d2e6511977fedb
Hardcoder <proteus.dial8891@gmail.com>
2026-05-25 15:15:24 +0100

    Added retries to ensure mp has started a new socket after finishing playback of a track.

--- player_to_mp_bridge/src/audio.rs
+++ player_to_mp_bridge/src/audio.rs
@@ -15,7 +15,7 @@ use std::sync::{Arc,Mutex};
 use std::path::Path;
 use std::fs::File;
 use std::io::prelude::*;
-use std::net::TcpStream;
+use std::net::{TcpStream, Shutdown};
 use std::thread::{sleep};
 use std::time::{Duration, Instant};
 use std::io::Write;
@@ -62,11 +62,10 @@ pub fn play(path: &str, sink: Arc<Sink>)
 	if path_s.try_exists().unwrap()
 	{
 		let mut input = File::open(path_s).unwrap();
-		let address = options::getMicroPolarityAddress();
-		let mut stream = TcpStream::connect(address.as_str()).unwrap();
 		let mut buffer = [0u8; 90112/(2)];
+		let mut ready_buffer = [0];
 
-		let mut retry_count = 5;
+		let mut retry_count = 8;
 		let mut profile_count = 4;
 		let mut offset = 0;
 		let mut written = 0;
@@ -75,40 +74,56 @@ pub fn play(path: &str, sink: Arc<Sink>)
 		let mut profile_start = Instant::now();
 		let mut read = input.read(&mut buffer).unwrap();
 
-		println!("TCP Stream read timmeout : {:?} and write timeout : {:?}", 
-			stream.read_timeout(), stream.write_timeout());
+		let address = options::getMicroPolarityAddress();
 
-		while read > 0 {
-			written = match stream.write(&buffer[..(read + offset)]) {
-				Ok(n) => { retry_count = 5; n},
-				Err(e) => {
-					println!("Error {:?}", e);
-					if retry_count > 0 {
-						println!("Retrying {}/5", 5 - retry_count + 1);
-						retry_count -= 1;
-						continue;
-					} else {
-						break;
+		while retry_count > 0 {
+			match TcpStream::connect(address.as_str()) {
+				Ok(mut stream) => {
+					println!("TCP Stream read timmeout : {:?} and write timeout : {:?}", 
+						stream.read_timeout(), stream.write_timeout());
+
+					while read > 0 {
+						written = match stream.write(&buffer[..(read + offset)]) {
+							Ok(n) => {n},
+							Err(e) => {
+								println!("Error writing to mp device. Terminating.");
+								return
+							}
+						};
+						written_sum += written;
+						//sleep(Duration::from_millis(5632/(2*2*2*2)));
+						buffer.copy_within(written..read,0);
+						offset = read - written;
+						if profile_count == 0 {
+							profile_duration = Instant::now().duration_since(profile_start).as_millis();
+							println!("Read : {} , Written : {} , Offset : {} @ {:.3}Kbps", read, written, offset,
+								(written_sum as f32)*2f32/(profile_duration as f32));
+							profile_count = 4;
+							written_sum = 0;
+							profile_duration = Duration::ZERO.as_millis();
+							profile_start = Instant::now();
+						}
+						read = input.read(&mut buffer[offset..]).unwrap();
+						profile_count -= 1;
 					}
-				}
-			};
-			written_sum += written;
-			//sleep(Duration::from_millis(5632/(2*2*2*2)));
-			buffer.copy_within(written..read,0);
-			offset = read - written;
-			if profile_count == 0 {
-				profile_duration = Instant::now().duration_since(profile_start).as_millis();
-				println!("Read : {} , Written : {} , Offset : {} @ {:.3}Kbps", read, written, offset,
-					(written_sum as f32)*2f32/(profile_duration as f32));
-				profile_count = 4;
-				written_sum = 0;
-				profile_duration = Duration::ZERO.as_millis();
-				profile_start = Instant::now();
-			}
-			read = input.read(&mut buffer[offset..]).unwrap();
-			profile_count -= 1;
-		}
+				
+					println!("Shutting down.");
+					stream.shutdown(Shutdown::Both).expect("Shutdown failed.");
+					// println!("Waiting for connection reset.");
+					// while let Ok(_) = stream.read(&mut ready_buffer) {
+					// 	sleep(Duration::from_millis(500));
+					// }
 
+					break;
+				},
+
+				Err(_) => {
+					sleep(Duration::from_millis(500));
+					retry_count -= 1;
+					continue;
+				}
+			}
+		}
 	}
 	// else {
 	// 	std::fs::create_dir_all(path_s.parent().unwrap()).unwrap();
